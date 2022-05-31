88b8afc5655cd5f255bcbc2063b5e0424b887441
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2022-05-31 10:11:53 -0400

    Add files via upload

---
+++ Dynamic Programming/Stone Game.cpp
@@ -0,0 +1,23 @@
+// in the name of Allah
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sync ios_base :: sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#pragma GCC optimize("O0")
+#pragma GCC optimize("O1")
+
+int main() {
+  sync;
+  int TC; cin >> TC;
+  while (TC--) {
+       int n; cin >> n;
+       map<int, int>A;
+       for (int i = 1; i <= n; i++) {
+	    int a; cin >> a, A[a]=i;
+       }
+       int x, y; if (A[n]>A[1]) y=A[n], x=A[1]; else y=A[1], x=A[n]; n++;
+       cout << min((x-0)+(n-y), min((y-x)+(n-y), (y-x)+(x-0))) << endl;
+  }
+}
